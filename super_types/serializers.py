358a6d03bad8b9c8cc0109ad87d8dcb368be149a
C-Skala <cjskala@gmail.com>
2022-07-21 22:34:52 -0400

    rough model and serilaizers made

---
+++ super_types/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Super_Types
+
+class Super_types_serializers (serializers.ModelSerializer):
+    class Meta:
+        model = Super_Types
+        fields = ['id', 'type']
--- supers/models.py
+++ supers/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Supers(models.Model):
+    name = models.CharField(max_length= 200)
+    alter_ego = models.CharField(max_length=200)
+    Primary_ability = models.CharField(max_length= 200)
+    secondary_ability = models.CharField(max_length= 200)
+    catchphrase = models.CharField(max_length= 200)
+    super_type = forign key
